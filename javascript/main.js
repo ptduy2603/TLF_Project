60e5e98fc13b14c7be343a97e7cad91e76449720
Duong0907 <thanhduongphan12345@gmail.com>
2023-05-26 23:32:49 +0700

    add login feature

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,29 @@
-console.log("Hello world!")
+// Slide show
+
+let slideIndex = 0;
+slideshow = document.getElementById('content-slide');
+let numberOfSlides = 3;
+function showSlide(i = 0) {
+    i = i % numberOfSlides;
+    slideshow.setAttribute('style', `
+        background-image: url("../assets/images/surgery${i + 1}.jpg");
+    `);
+
+    setTimeout(() => {
+        showSlide(i + 1);
+        console.log("OK");
+    }, 3000);
+}
+
+showSlide();
+
+
+// Login modal
+var modal = document.getElementById('login-modal');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
